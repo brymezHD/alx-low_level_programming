11bb356b29855aee8acaacee53a18f1a014832b4
Oseni Ibrahim <hussainhebraheem@gmail.com>
2023-03-16 07:18:24 -0700

    better dats

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+ * grammer
+ */
 int main(void)
 {
-    printf("with proper grammar\, but the outcome is a piece of art\,");
-    return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
