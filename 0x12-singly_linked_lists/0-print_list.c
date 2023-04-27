641123a1a2ec493b6a98b66c8381fbea86bb70d2
brymezHD <hussainhebraheem@gmail.com>
2023-04-27 11:28:51 -0700

    new commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: A pointer to the list_t list.
- *
  * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
@@ -16,6 +15,5 @@ size_t print_list(const list_t *h)
         h = h->next;
         count++;
     }
-
     return (count);
 }
